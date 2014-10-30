4888ba24f32c32c17c4ff14debb39ee80cf11e17
Sam Stainsby <sam@stainsby.id.au>
2014-10-30 13:54:53 +1000

    move bootstrap rebuild out of deploy script and into build script

--- theme-seed/build.sh
+++ theme-seed/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-rm -rf release/*
-mkdir -p release
-cp src/thumbnail.png release
-cp src/site.xml release
-cd src
-zip -x 'resources/bower_components/bootstrap/node_modules/*' -r ../release/theme.zip .
+rm -rf release/* && \
+  mkdir -p release && \
+  cp src/thumbnail.png release && \
+  cp src/site.xml release && \
+  cd src && \
+  ./rebuild_bootstrap.sh && \
+  zip -x 'resources/bower_components/bootstrap/node_modules/*' -r ../release/theme.zip .
